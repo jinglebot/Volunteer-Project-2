128048ee07a05c457ae532ba855b134f3b8762c1
jinglebot <jingle1216@yahoo.com>
2016-10-24 17:13:20 -0700

    Change layout, fonts, carousel, navbar, add navbar, modal songlist, links, footer

--- js/carousel.js
+++ js/carousel.js
@@ -1,27 +1,40 @@
-	var testi_idx = 0;
-	var testi_elem;
-	var testi_arr = [
-		"<h5>Art of the Gun</h5><div><h6>Song 1: put catchy song verse here </h6><small>Harry Herrera, John Herrera, <cite>San Francisco, CA 2015</cite></small></div></div>",
-		"<h5>Chess</h5><div><h6>Song 2</h6><small>John Herrera, Harry Herrera, <cite>San Francisco, CA 2014</cite></small></div>",
-		"<h5>Dead on Arrival</h5><div><h6>Song 3</h6><small>Harry Herrera, John Herrera, <cite>San Francisco, CA 2013</cite></small></div>",
-		"<h5>I will be free</h5><div><h6>Song 4</h6><small>John Herrera, <cite>San Francisco, CA 2015</cite></small></div>",
-		"<h5>It’s you</h5><div><h6>Song 5</h6><small>John Herrera, <cite>San Francisco, CA 2014</cite></small></div>",
-		"<h5>Love hits me down</h5><div><h6>Song 6</h6><small>John Herrera, <cite>San Francisco, CA 2013</cite></small></div>",
-		"<h5>Passing Rites</h5><div><h6>Song 7</h6><small>John Herrera, <cite>San Francisco, CA 2015</cite></small></div>",
-		"<h5>The Race</h5><div><h6>Song 8</h6><small>Harry Herrera, John Herrera, <cite>San Francisco, CA 2014</cite></small></div>",
-		"<h5>Twisted Scenery</h5><div><h6>Song 9</h6><small>Harry Herrera, John Herrera, <cite>San Francisco, CA 2013</cite></small></div>",
-		"<h5>Winner</h5><div><h6>Song 10</h6><small>Harry Herrera, John Herrera, <cite>San Francisco, CA 2014</cite></small></div>"
-	];
-	function testi_next () {
-		testi_idx++;
-		testi_elem.style.opacity = 0;
-		setTimeout('testi_ofOne()', 7000);
-		if (testi_idx > (testi_arr.length - 1)) {
-			testi_idx = 0;
-		}
+	// var testi_idx = 0;
+	// var testi_elem;
+	// var testi_arr = [
+	// 	"<h5>Art of the Gun</h5><div><h6>Song 1: put catchy song verse here </h6><small>Harry Herrera, John Herrera, <cite>San Francisco, CA 2015</cite></small></div></div>",
+	// 	"<h5>Chess</h5><div><h6>Song 2</h6><small>John Herrera, Harry Herrera, <cite>San Francisco, CA 2014</cite></small></div>",
+	// 	"<h5>Dead on Arrival</h5><div><h6>Song 3</h6><small>Harry Herrera, John Herrera, <cite>San Francisco, CA 2013</cite></small></div>",
+	// 	"<h5>I will be free</h5><div><h6>Song 4</h6><small>John Herrera, <cite>San Francisco, CA 2015</cite></small></div>",
+	// 	"<h5>It’s you</h5><div><h6>Song 5</h6><small>John Herrera, <cite>San Francisco, CA 2014</cite></small></div>",
+	// 	"<h5>Love hits me down</h5><div><h6>Song 6</h6><small>John Herrera, <cite>San Francisco, CA 2013</cite></small></div>",
+	// 	"<h5>Passing Rites</h5><div><h6>Song 7</h6><small>John Herrera, <cite>San Francisco, CA 2015</cite></small></div>",
+	// 	"<h5>The Race</h5><div><h6>Song 8</h6><small>Harry Herrera, John Herrera, <cite>San Francisco, CA 2014</cite></small></div>",
+	// 	"<h5>Twisted Scenery</h5><div><h6>Song 9</h6><small>Harry Herrera, John Herrera, <cite>San Francisco, CA 2013</cite></small></div>",
+	// 	"<h5>Winner</h5><div><h6>Song 10</h6><small>Harry Herrera, John Herrera, <cite>San Francisco, CA 2014</cite></small></div>"
+	// ];
+	// function testi_next () {
+	// 	testi_idx++;
+	// 	testi_elem.style.opacity = 0;
+	// 	setTimeout('testi_ofOne()', 7000);
+	// 	if (testi_idx > (testi_arr.length - 1)) {
+	// 		testi_idx = 0;
+	// 	}
+	// }
+	// function testi_ofOne () {
+	// 	testi_elem.innerHTML = testi_arr[testi_idx];
+	// 	testi_elem.style.opacity = 1;
+	// 	setTimeout('testi_next()', 7000);
+	// }
+
+var index = 0;
+var carousel-elem;
+function nextSlide () {
+	index++;
+	// setTimeout ( 'currentSlide ()', 4000 );
+	if ( index > (songsArray.length - 1) ) {
+			index = 0;
 	}
-	function testi_ofOne () {
-		testi_elem.innerHTML = testi_arr[testi_idx];
-		testi_elem.style.opacity = 1;
-		setTimeout('testi_next()', 7000);
-	}
+}
+function currentSlide () {
+	carousel-elem.text = songsArray[index];
+}
--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,106 @@
 // MODEL
 var songs = [{
+	songId: "song1",
 	songNum: "#song1",
-	title: "Art of the Gun"
+	title: "Art of the Gun",
+	verse: "You have my heart inside of your hand...",
+	author: "Harry Herrera, John Herrera",
+	location: "San Francisco, CA",
+	year: 2015,
+	visibility: "hidden",
+	reflection: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
 	}, {
+	songId: "song2",
 	songNum: "#song2",
-	title: "Chess"
+	title: "Chess",
+	verse: "You have my heart inside of your hand...",
+	author: "Harry Herrera, John Herrera",
+	location: "San Francisco, CA",
+	year: 2014,
+	visibility: "hidden",
+	reflection: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
 	}, {
+	songId: "song3",
 	songNum: "#song3",
-	title: "Dead on Arrival"
+	title: "Dead on Arrival",
+	verse: "You have my heart inside of your hand...",
+	author: "Harry Herrera, John Herrera",
+	location: "San Francisco, CA",
+	year: 2013,
+	visibility: "hidden",
+	reflection: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
 	}, {
+	songId: "song4",
 	songNum: "#song4",
-	title: "I will be free"
+	title: "I will be free",
+	verse: "You have my heart inside of your hand...",
+	author: "John Herrera",
+	location: "San Francisco, CA",
+	year: 2015,
+	visibility: "hidden",
+	reflection: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
 	}, {
+	songId: "song5",
 	songNum: "#song5",
-	title: "It’s you"
+	title: "It’s you",
+	verse: "You have my heart inside of your hand...",
+	author: "John Herrera",
+	location: "San Francisco, CA",
+	year: 2014,
+	visibility: "hidden",
+	reflection: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
 	}, {
+	songId: "song6",
 	songNum: "#song6",
-	title: "Love hits me down"
+	title: "Love hits me down",
+	verse: "You have my heart inside of your hand...",
+	author: "John Herrera",
+	location: "San Francisco, CA",
+	year: 2013,
+	visibility: "hidden",
+	reflection: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
 	}, {
+	songId: "song7",
 	songNum: "#song7",
-	title: "Passing Rites"
+	title: "Passing Rites",
+	verse: "You have my heart inside of your hand...",
+	author: "John Herrera",
+	location: "San Francisco, CA",
+	year: 2015,
+	visibility: "hidden",
+	reflection: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
 	}, {
+	songId: "song8",
 	songNum: "#song8",
-	title: "The Race"
+	title: "The Race",
+	verse: "You have my heart inside of your hand...",
+	author: "Harry Herrera, John Herrera",
+	location: "San Francisco, CA",
+	year: 2014,
+	visibility: "hidden",
+	reflection: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
 	}, {
+	songId: "song9",
 	songNum: "#song9",
-	title: "Twisted Scenery"
+	title: "Twisted Scenery",
+	verse: "You have my heart inside of your hand...",
+	author: "Harry Herrera, John Herrera",
+	location: "San Francisco, CA",
+	year: 2013,
+	visibility: "hidden",
+	reflection: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
 	}, {
+	songId: "song10",
 	songNum: "#song10",
-	title: "Winner"
+	title: "Winner",
+	verse: "You have my heart inside of your hand...",
+	author: "Harry Herrera, John Herrera",
+	location: "San Francisco, CA",
+	year: 2014,
+	visibility: "hidden",
+	reflection: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
 	}];
+
 
 // VIEWMODEL
 var Viewmodel = function () {
@@ -38,8 +109,50 @@ var Viewmodel = function () {
 	songs.forEach(function(song) {
 		self.songsArray.push(song);
 	});
-	this.songList = ko.computed(function () {
+	console.log(self.songsArray());
+	// this.displayDivider = ko.observable( function(index) {
+	// 	console.log ($index);
+	// 	return (true);
+	// });
+
+
+	    this.carouselList = ko.computed(function() {
+	        return ko.utils.arrayFilter(self.songsArray(), function(song) {
+	            // $('.carousel-indicators li:first').addClass('active');
+	                // song.visibility = "visible";
+	    //             locate.marker.setMap(map);
+	    //             bounds.extend(locate.marker.position);
+	    //             // console.log(locate.marker.position);
+	    //             map.fitBounds(bounds);
+	    //             map.setCenter(bounds.getCenter());
+	    // console.log("index = " + $index());
+	            return song;
+	    //         } else {
+	    //             locate.marker.setVisible(false);
+	    //             locate.marker.setMap(null);
+	    //             return false;
+	    //         }
+	        });
+	    });
+
+	    // this.checkIndex = ko.computed(function () {
+	    	// if ($index == 0) {
+			    // $('div div').addClass('active');
+			// }
+	    // });
+		$('.carousel-indicators li').addClass('active');
+		$('.carousel-inner div div').addClass('active');
+    // (song.index.indexOf(self.searchBar().toLowerCase()) >= 0)) {
+
+
+
+
+
+	this.navSongList = ko.computed(function () {
 		return ko.utils.arrayFilter(self.songsArray(), function (song) {
+
+
+
 		});
 	});
 };
