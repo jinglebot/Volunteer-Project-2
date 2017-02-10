166e4e2be64f43e5e1d1d0bfe3cff648cc5ff4bc
jinglebot <jingle1216@yahoo.com>
2017-02-09 17:27:14 -0800

    Update layout, data and fonts

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ var songs = [{
 	songId: "song1",
 	songNum: "#song1",
 	title: "Art of the Gun",
-	verse: "You have my heart inside of your hand...",
+	verse: "Do you wait to see if my love battles all",
 	author: "Harry Herrera, John Herrera",
 	location: "San Francisco, CA",
 	year: 2015,
@@ -13,7 +13,7 @@ var songs = [{
 	songId: "song2",
 	songNum: "#song2",
 	title: "Chess",
-	verse: "You have my heart inside of your hand...",
+	verse: "Make the right move or die",
 	author: "Harry Herrera, John Herrera",
 	location: "San Francisco, CA",
 	year: 2014,
@@ -23,7 +23,7 @@ var songs = [{
 	songId: "song3",
 	songNum: "#song3",
 	title: "Dead on Arrival",
-	verse: "You have my heart inside of your hand...",
+	verse: "Life is drawn like a tiger without soul",
 	author: "Harry Herrera, John Herrera",
 	location: "San Francisco, CA",
 	year: 2013,
@@ -32,8 +32,8 @@ var songs = [{
 	}, {
 	songId: "song4",
 	songNum: "#song4",
-	title: "I will be free",
-	verse: "You have my heart inside of your hand...",
+	title: "I will be Free",
+	verse: "I never thought to look for it from the one that gave me life",
 	author: "John Herrera",
 	location: "San Francisco, CA",
 	year: 2015,
@@ -43,7 +43,7 @@ var songs = [{
 	songId: "song5",
 	songNum: "#song5",
 	title: "It’s you",
-	verse: "You have my heart inside of your hand...",
+	verse: "Knowing our love means it, we have to rock it loose",
 	author: "John Herrera",
 	location: "San Francisco, CA",
 	year: 2014,
@@ -52,28 +52,28 @@ var songs = [{
 	}, {
 	songId: "song6",
 	songNum: "#song6",
-	title: "Love hits me down",
-	verse: "You have my heart inside of your hand...",
-	author: "John Herrera",
-	location: "San Francisco, CA",
-	year: 2013,
-	visibility: "hidden",
-	reflection: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
-	}, {
-	songId: "song7",
-	songNum: "#song7",
 	title: "Passing Rites",
-	verse: "You have my heart inside of your hand...",
+	verse: "My eyes were wide open looking for adventure in a dangerous world",
 	author: "John Herrera",
 	location: "San Francisco, CA",
 	year: 2015,
 	visibility: "hidden",
 	reflection: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
 	}, {
+	songId: "song7",
+	songNum: "#song7",
+	title: "Twisted Scenery",
+	verse: "Time runs dry, the desert, the sky full of moons",
+	author: "Harry Herrera, John Herrera",
+	location: "San Francisco, CA",
+	year: 2013,
+	visibility: "hidden",
+	reflection: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
+	}, {
 	songId: "song8",
 	songNum: "#song8",
 	title: "The Race",
-	verse: "You have my heart inside of your hand...",
+	verse: "I won my medal of honor, I will come for more",
 	author: "Harry Herrera, John Herrera",
 	location: "San Francisco, CA",
 	year: 2014,
@@ -82,21 +82,21 @@ var songs = [{
 	}, {
 	songId: "song9",
 	songNum: "#song9",
-	title: "Twisted Scenery",
-	verse: "You have my heart inside of your hand...",
+	title: "Winner",
+	verse: "One minute you're here then you are gone, anyone can take your life",
 	author: "Harry Herrera, John Herrera",
 	location: "San Francisco, CA",
-	year: 2013,
+	year: 2014,
 	visibility: "hidden",
 	reflection: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
 	}, {
 	songId: "song10",
 	songNum: "#song10",
-	title: "Winner",
-	verse: "You have my heart inside of your hand...",
-	author: "Harry Herrera, John Herrera",
+	title: "Love hits me down",
+	verse: "Dreams are the ones alive",
+	author: "John Herrera",
 	location: "San Francisco, CA",
-	year: 2014,
+	year: 2013,
 	visibility: "hidden",
 	reflection: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
 	}];
@@ -110,39 +110,15 @@ var Viewmodel = function () {
 		self.songsArray.push(song);
 	});
 	console.log(self.songsArray());
-	// this.displayDivider = ko.observable( function(index) {
-	// 	console.log ($index);
-	// 	return (true);
-	// });
 
+    this.carouselList = ko.computed(function() {
+        return ko.utils.arrayFilter(self.songsArray(), function(song) {
+            return song;
+        });
+    });
 
-	    this.carouselList = ko.computed(function() {
-	        return ko.utils.arrayFilter(self.songsArray(), function(song) {
-	            // $('.carousel-indicators li:first').addClass('active');
-	                // song.visibility = "visible";
-	    //             locate.marker.setMap(map);
-	    //             bounds.extend(locate.marker.position);
-	    //             // console.log(locate.marker.position);
-	    //             map.fitBounds(bounds);
-	    //             map.setCenter(bounds.getCenter());
-	    // console.log("index = " + $index());
-	            return song;
-	    //         } else {
-	    //             locate.marker.setVisible(false);
-	    //             locate.marker.setMap(null);
-	    //             return false;
-	    //         }
-	        });
-	    });
-
-	    // this.checkIndex = ko.computed(function () {
-	    	// if ($index == 0) {
-			    // $('div div').addClass('active');
-			// }
-	    // });
-		$('.carousel-indicators li').addClass('active');
-		$('.carousel-inner div div').addClass('active');
-    // (song.index.indexOf(self.searchBar().toLowerCase()) >= 0)) {
+	$('.carousel-indicators li').addClass('active');
+	$('.carousel-inner div div').addClass('active');
 
 
 
